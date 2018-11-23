1c1aaccef7c05c7939af3df74b5dc418dd2069d5
Ken Hayashi <hayashi58@gmail.com>
2018-11-23 15:40:20 +0900

    第３コミット

--- Git_sample/ViewController.swift
+++ Git_sample/ViewController.swift
@@ -14,17 +14,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
-        textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        textLabel.backgroundColor = UIColor.red
-        textLabel.text = "TECH::CAMP"
-        textLabel.font = UIFont.systemFont(ofSize: 40)
-        textLabel.textColor = UIColor.white
-        textLabel.shadowColor = UIColor.blue
-        textLabel.textAlignment = NSTextAlignment.center
-        textLabel.layer.masksToBounds = true
-        textLabel.layer.cornerRadius = 20.0
+        let textLabel = makeTextLable()
         self.view.addSubview(textLabel)
+    }
+        
+        func makeTextLable() -> UILabel {
+            let textLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+            textLabel.layer.position = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
+            textLabel.backgroundColor = UIColor.red
+            textLabel.text = "TECH::CAMP"
+            textLabel.font = UIFont.systemFont(ofSize: 40)
+            textLabel.textColor = UIColor.white
+            textLabel.shadowColor = UIColor.blue
+            textLabel.textAlignment = NSTextAlignment.center
+            textLabel.layer.masksToBounds = true
+            textLabel.layer.cornerRadius = 20.0
+            return textLabel
     }
 
     override func didReceiveMemoryWarning() {
